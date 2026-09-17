b5b14b972f513055c8f4a885c8328615c536e174
Jeneel Dumasia <jeneel.dumasia@itara.io>
2026-09-18 01:30:18 +0530

    fix: disable deletion_protection to allow cluster rebuild

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,6 +94,7 @@ resource "google_container_cluster" "primary" {
 
   remove_default_node_pool = true
   initial_node_count       = 1
+  deletion_protection      = false  # Allow cluster destruction for rebuild
 
   # Workload Identity enabled for the cluster
   workload_identity_config {
