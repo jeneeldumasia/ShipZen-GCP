57e2e70a043f5959f23420a9cbe82ff43a4d54ab
Jeneel-Itara <jeneel.dumasia@iamops.io>
2026-09-21 03:01:22 +0530

    Fix terraform deletion protection and Cloudflare DNS type

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,7 +94,7 @@ resource "google_container_cluster" "primary" {
 
   remove_default_node_pool = true
   initial_node_count       = 1
-  deletion_protection      = true # Prevent accidental cluster destruction
+  deletion_protection      = false # Prevent accidental cluster destruction
 
   # Workload Identity enabled for the cluster
   workload_identity_config {
