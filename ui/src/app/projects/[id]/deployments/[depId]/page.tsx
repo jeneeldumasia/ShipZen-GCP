3d0b1c9e0115fe877574bdc45a93d169b21403c2
Jeneel-Itara <jeneel.dumasia@iamops.io>
2026-09-23 09:39:48 +0530

    fix(ui): correct typescript type comparison for Running state

--- ui/src/app/projects/[id]/deployments/[depId]/page.tsx
+++ ui/src/app/projects/[id]/deployments/[depId]/page.tsx
@@ -97,7 +97,7 @@ export default async function DeploymentPage(props: { params: Promise<{ id: stri
       </div>
 
       {/* The Pulse */}
-      {deployment.state === "Deployed" && <Pulse state={deployment.state} url={appUrl} />}
+      {deployment.state === "Running" && <Pulse state={deployment.state} url={appUrl} />}
 
       {/* Error state */}
       {deployment.state === "Failed" && deployment.last_error && (
