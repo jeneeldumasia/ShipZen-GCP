b3c3fa1db1f4d8b7d64a9b310fc3cd6cd6e23d1f
Jeneel-Itara <jeneel.dumasia@iamops.io>
2026-09-23 07:35:20 +0530

    fix(terraform): add ABANDON deletion policy to service networking connection
    This prevents teardown pipelines from failing due to GCP background cleanup delays when tearing down Cloud SQL.

--- terraform/postgres.tf
+++ terraform/postgres.tf
@@ -27,6 +27,7 @@ resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.vpc.id
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address[0].name]
+  deletion_policy         = "ABANDON"
 }
 
 resource "google_sql_database_instance" "postgres" {
